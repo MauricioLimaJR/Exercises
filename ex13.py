c9013035377abb2997b4f592788f2ccb9866a289
Maurício de Lima <jmauriciolm38@gmail.com>
2016-12-26 16:49:07 -0300

    ex13.py

--- ex13.py
+++ ex13.py
@@ -1,8 +1,10 @@
 from sys import argv
 
 script, first, second, third = argv
+user = raw_input()
 
 print "The script id called:", script
 print "Yout first variable is:", first
 print "Your second variable is:", second
 print "your third variable is:", third
+print "Yout user variable is:", user
