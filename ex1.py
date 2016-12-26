7e7f1e05ad5ba97c094376b3c96815f124071132
Maurício de Lima <jmauriciolm38@gmail.com>
2016-12-25 22:39:22 -0300

    LPTHW exercises

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again!"
+print "I like typing this"
+print "This is fun."
+print 'Yay! printing.'
+print "I'd much tather you not"
+print 'I "said" do not touch this.'
+# -*- coding: utf-8 -*-
